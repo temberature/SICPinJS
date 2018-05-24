fe805f3ac167b59216b29eadd51111bae02ac31c
Tong <temberature@gmail.com>
2018-05-24 16:23:38 +0800

    改善可读性。

--- src/sqrt.js
+++ src/sqrt.js
@@ -1,6 +1,9 @@
 var abs = function(num) {
   return num > 0 ? num : -num;
 };
+var average = function(x, y) {
+  return (x + y) / 2;
+}
 var sqrt = function(x) {
   if (typeof x !== "number" || x < 0) {
     throw new Error("input should be number and greater than 0");
@@ -9,7 +12,7 @@ var sqrt = function(x) {
     return abs(guess * guess - x) < 0.001;
   };
   var improve = function(guess) {
-    return (x / guess + guess) / 2;
+    return average(x / guess, guess);
   };
   var sqrtIter = function(guess, x) {
     return goodEnough(guess) ? guess : sqrtIter(improve(guess), x);
