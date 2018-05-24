2f44d36c7aeb092d5440d84fc215a64af0f8865e
Tong <temberature@gmail.com>
2018-05-24 18:01:54 +0800

    迭代式。

--- src/factorial.js
+++ src/factorial.js
@@ -1,11 +1,17 @@
 var factorial = function(n) {
-  if (typeof n !== 'number' || n < 0 || n !== parseInt(n, 10)) {
-    throw new Error('input should be integer.');
+  if (typeof n !== "number" || n < 0 || n !== parseInt(n, 10)) {
+    throw new Error("input should be integer.");
   }
-  if (n < 2) {
+  if (n < 1) {
     return n;
   }
-  return n * factorial(n - 1)
-}
+  var factIter = function(counter, product) {
+    if (counter < 2) {
+      return product;
+    }
+    return factIter(counter - 1, product * counter);
+  };
+  return factIter(n, 1);
+};
 
 module.exports = factorial;
