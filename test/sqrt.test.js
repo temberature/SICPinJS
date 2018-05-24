ae582ded23144158a78e63ca1745f772d7ad76b0
Tong <temberature@gmail.com>
2018-05-24 17:03:53 +0800

    丰富测试用例。

--- test/sqrt.test.js
+++ test/sqrt.test.js
@@ -3,9 +3,21 @@ var expect = require("chai").expect;
 
 describe("开平方", function() {
   it("根号2约等于1.4", function() {
-    var r = +sqrt(2).toFixed(1);
-    expect(r).to.equal(1.4);
+    var r = sqrt(2);
+    expect(r).to.be.closeTo(1.4, 0.1);
   });
+  it("根号4等于2", function() {
+    var r = sqrt(4);
+    expect(r).to.equal(2);
+  });
+  it("根号0.00009约等于0.009", function() {
+    var r = sqrt(0.00009);
+    expect(r).to.be.closeTo(0.009, 0.001);
+  });
+  // it("根号900000000000000000000000000000000000000000000000000000000000000000000000000000000000约等于9.486832980505138e+41", function() {
+  //   var r = sqrt(900000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+  //   expect(r).to.be.closeTo(9.486832980505138e+41, 1e+41);
+  // });
   it("根号0等于0", function() {
     var r = sqrt(0);
     expect(r).to.equal(0);
