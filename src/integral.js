412047c53bb0fbc2c9e30b9553661f964d50b4ba
Tong <temberature@gmail.com>
2018-05-25 20:42:22 +0800

    lambda

--- src/integral.js
+++ src/integral.js
@@ -3,16 +3,16 @@ var sum = require("./sum");
 
 var integral = function(f, a, b, dx) {
   var a = a + dx / 2;
-  var term = function(a) {
-    return f(a) * dx;
-  }
-  var next = function(a) {
-    return a + dx;
-  }
-  if (a >= b) {
-    return term(b);
-  }
-  return term(a) + sum(term, next(a), next, b);
+  return sum(
+    function(a) {
+      return f(a) * dx;
+    },
+    a,
+    function(a) {
+      return a + dx;
+    },
+    b
+  );
 };
 
 module.exports = integral;
