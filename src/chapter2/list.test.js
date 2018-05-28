6312ae70a2f61398c12dc86f8dd59e05a0bbe4e4
Tong <temberature@gmail.com>
2018-05-28 16:49:46 +0800

    增加list 测试用例。

--- src/chapter2/list.test.js
+++ src/chapter2/list.test.js
@@ -1,5 +1,7 @@
 var expect = require("chai").expect;
+var cons = require("./cons").cons;
 var car = require("./cons").car;
+var cdr = require("./cons").cdr;
 var list = require("./list").list;
 var equalList = require("./list").equalList;
 
@@ -12,5 +14,21 @@ describe("list", function() {
   it("car(oneThroughFour) === 1", function() {
     var r = car(oneThroughFour);
     expect(r).to.equal(1);
+  });
+  it("cdr(oneThroughFour) === list(2, 3, 4)", function() {
+    var r = equalList(cdr(oneThroughFour), list(2, 3, 4));
+    expect(r).to.equal(true);
+  });
+  it("car(cdr(oneThroughFour)) === 2", function() {
+    var r = car(cdr(oneThroughFour));
+    expect(r).to.equal(2);
+  });
+  it("cons(10, oneThroughFour) === list(10, 1, 2, 3, 4)", function() {
+    var r = equalList(cons(10, oneThroughFour), list(10, 1, 2, 3, 4));
+    expect(r).to.equal(true);
+  });
+  it("cons(5, oneThroughFour) === list(5, 1, 2, 3, 4)", function() {
+    var r = equalList(cons(5, oneThroughFour), list(5, 1, 2, 3, 4));
+    expect(r).to.equal(true);
   });
 });
